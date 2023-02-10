81981c7b8a7bc98c67f7218c35794193b380429f
Mark Wilcox <mark.wilcox@olioex.com>
2023-02-10 15:58:37 +0000

    Add filtering for business type and specific names

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ import axios from 'axios';
 import fs from 'fs';
 
 let establishments = [];
+const nameFilter = [
+  'Elior',
+  'Lexington',
+  'Taylor Shaw',
+  'Caterplus',
+  'Edwards and Blake',
+  'Genuine Dining',
+  'Wilson Vale',
+  'Bartlett Mitchell',
+  'ISS',
+  'RA group',
+  'Levy',
+  'Chartwells',
+  'Eurest',
+  '14Forty',
+  'Compass',
+  'Baxter Storey',
+  'Grazing',
+  'OCS',
+  'Thomas Franks',
+  'Atalian Servest',
+  'Bennet Hay',
+  'CH&Co',
+  'Vacherin',
+  'KSG',
+  'Fooditude',
+  'Connect Vending',
+];
 
 const getAuthorityIds = async () => {
   const authorities = await axios.get('https://api.ratings.food.gov.uk/Authorities/basic', { headers: {
@@ -14,14 +42,22 @@ const getAuthorityIds = async () => {
 const getEstablishments = async () => {
   const authorityIds = await getAuthorityIds();
   for (const authorityId of authorityIds) {
-    const authorityEstabilshments = await axios.get(`https://api.ratings.food.gov.uk/Establishments/?localAuthorityId=${authorityId}`, { headers: {
+    const authorityEstabilshments = await axios.get(`https://api.ratings.food.gov.uk/Establishments/?localAuthorityId=${authorityId}&businessTypeId=1`, { headers: {
       "x-api-version": 2,
     }});
     const transformedEstablishments = authorityEstabilshments.data.establishments.map(
       ({ BusinessName , BusinessType, BusinessTypeID, AddressLine1, AddressLine2, AddressLine3, AddressLine4, PostCode, Phone }) => {
         return { BusinessName , BusinessType, BusinessTypeID, AddressLine1, AddressLine2, AddressLine3, AddressLine4, PostCode, Phone };
       })
-    establishments = establishments.concat(transformedEstablishments);
+    const filteredEstablishments = transformedEstablishments.filter(establishment => {
+      for(const name of nameFilter) {
+        if (establishment.BusinessName.includes(name)) {
+          return true;
+        }
+      }
+      return false;
+    })
+    establishments = establishments.concat(filteredEstablishments);
     console.log(`Finished authority ID: ${authorityId}`);
   }
 }
